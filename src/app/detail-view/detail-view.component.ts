f9ebf12720153b4042467f3027d80f62e5d5c700
Avidelux <avidelux-x@web.de>
2019-06-20 16:20:14 +0200

    get single document

--- src/app/detail-view/detail-view.component.ts
+++ src/app/detail-view/detail-view.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EntryService } from '../entry.service';
+import { Entry } from '../entry.model';
 
 @Component({
   selector: 'app-detail-view',
   templateUrl: './detail-view.component.html',
   styleUrls: ['./detail-view.component.css']
 })
+
 export class DetailViewComponent implements OnInit {
 
-  constructor() { }
+  entryID; 
 
+  constructor( private route: ActivatedRoute, private entryService: EntryService ) { }
+  
   ngOnInit() {
+    this.getID();
+
+  }
+
+  getID(){
+    this.entryID = this.route.snapshot.paramMap.get('id');
+    console.log(this.entryID);
+  }
+
+  displayEntry(id: string){ 
+    this.entryService.getEntry(id).get().toPromise().then(
+      function(documentSnapshot){
+        if(documentSnapshot.exists){
+          var data = documentSnapshot.data();
+          console.log(data);
+          console.log(
+           JSON.stringify(data));
+        }else{
+          console.log("document doesnt exist");
+        }
+      }
+    )
+      
+    
+
   }
 
 }
--- src/app/entry.service.ts
+++ src/app/entry.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Entry } from './entry.model';
+import { switchMap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntryService {
 
+  firebaseCollection = this.firestore.collection('entries');
+
   constructor( private firestore: AngularFirestore ) {   }
 
   getEntries(){ // returns all entries in the collection as an observable
-    return this.firestore.collection('entries').snapshotChanges();
+    return this.firebaseCollection.snapshotChanges();
   }
+
+  getEntry(id: string){ //returns specific document in the entries-collection
+    return this.firebaseCollection.doc(id);
+  }
+
+
 }
